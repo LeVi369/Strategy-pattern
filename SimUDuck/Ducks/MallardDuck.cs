ac9e3a45846506b96e393330e138beb8ea857e78
LeVi369 <91450176+LeVi369@users.noreply.github.com>
2022-02-17 13:14:21 +0300

    А

---
+++ SimUDuck/Ducks/MallardDuck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimUDuck
+{
+    public class MallardDuck : Duck
+    {
+        public override string Display()
+        {
+            return "Display" + " " + this.GetType();
+        }
+    }
+}
